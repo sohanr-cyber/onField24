5f9b733d98e815b4872415843411a342824753fd
sohanr-cyber <81401762+sohanr-cyber@users.noreply.github.com>
2024-09-23 10:44:16 +0600

    283053285

--- pages/index.js
+++ pages/index.js
@@ -23,117 +23,122 @@ export default function Home ({ data }) {
 
   return (
     <>
-      <div className={styles.wrapper}>
-        <div className={styles.categories}>
-          <List2 />
-        </div>
-        {data[0].articles.length > 0 && (
-          <>
-            <div className={styles.header__bigwidth}>
-              <Header article={data[0].articles[0]} />
+      {data.length > 0 && (
+        <div className={styles.wrapper}>
+          <div className={styles.categories}>
+            <List2 />
+          </div>
+          {data[0].articles.length > 0 && (
+            <>
+              <div className={styles.header__bigwidth}>
+                <Header article={data[0].articles[0]} />
+              </div>
+              <div className={styles.header__midwidth}>
+                <Header3 contents={[data[0].articles[0]]} />
+              </div>
+            </>
+          )}
+          {/* Latest Article */}
+          <div className={styles.latest__articles}>
+            <div className={styles.flex}>
+              <h2>{t('latest', lang)}</h2>
+              <div
+                className={styles.button}
+                onClick={() => router.push('/news')}
+              >
+                {t('seeAll', lang)} <EastIcon />
+              </div>
             </div>
-            <div className={styles.header__midwidth}>
-              <Header3 contents={[data[0].articles[0]]} />
-            </div>
-          </>
-        )}
-        {/* Latest Article */}
-        <div className={styles.latest__articles}>
-          <div className={styles.flex}>
-            <h2>{t('latest', lang)}</h2>
-            <div className={styles.button} onClick={() => router.push('/news')}>
-              {t('seeAll', lang)} <EastIcon />
+            <PBar height={'2px'} />
+            <div className={styles.grid}>
+              {data[0].articles.slice(1, 9).map((article, index) => (
+                <Article article={article} />
+              ))}
             </div>
           </div>
-          <PBar height={'2px'} />
-          <div className={styles.grid}>
-            {data[0].articles.slice(1, 9).map((article, index) => (
-              <Article article={article} />
-            ))}
-          </div>
-        </div>
-        <div className={styles.latest__articles}>
-          {' '}
-          <div className={styles.flex}>
-            <h2>{t('mustRead', lang)}</h2>
-            <div className={styles.button}>
-              {t('seeAll', lang)} <EastIcon />
+          <div className={styles.latest__articles}>
+            {' '}
+            <div className={styles.flex}>
+              <h2>{t('mustRead', lang)}</h2>
+              <div className={styles.button}>
+                {t('seeAll', lang)} <EastIcon />
+              </div>
             </div>
+            <PBar height={'2px'} />
           </div>
-          <PBar height={'2px'} />
-        </div>
-        {data[1].articles.length > 0 && (
-          <>
-            <Header3 contents={[data[1].articles[0]]} />
-            <div className={styles.articles}>
-              {data[0].articles
-                .slice(5, data[0].articles.length)
-                .map((article, index) => (
-                  <Article article={article} />
-                ))}
-            </div>
-          </>
-        )}
+          {data[1].articles.length > 0 && (
+            <>
+              <Header3 contents={[data[1].articles[0]]} />
+              <div className={styles.articles}>
+                {data[0].articles
+                  .slice(5, data[0].articles.length)
+                  .map((article, index) => (
+                    <Article article={article} />
+                  ))}
+              </div>
+            </>
+          )}
 
-        {/* By Category */}
-        {chunkArray(data.slice(2, data.length)).map((c, index) => (
-          <div className={styles.articles_bycategory}>
-            <div className={styles.left}>
-              <div className={styles.flex}>
-                <h2>{c[0]?.category}</h2>
-                <div className={styles.button}>
-                  See All <ArrowForwardIcon />{' '}
+          {/* By Category */}
+          {chunkArray(data.slice(2, data.length)).map((c, index) => (
+            <div className={styles.articles_bycategory}>
+              <div className={styles.left}>
+                <div className={styles.flex}>
+                  <h2>{c[0]?.category}</h2>
+                  <div className={styles.button}>
+                    See All <ArrowForwardIcon />{' '}
+                  </div>
+                </div>
+                <PBar height={'2px'} />
+                <div className={styles.grid2}>
+                  {c[0].articles?.map((article, index) => (
+                    <>
+                      {' '}
+                      <div className={styles.mediumToBig__width}>
+                        <Article
+                          article={article}
+                          style={{
+                            minWidth: '180px'
+                          }}
+                        />
+                      </div>
+                      <div className={styles.small__width}>
+                        <Article2 article={article} />
+                      </div>
+                    </>
+                  ))}
                 </div>
               </div>
-              <PBar height={'2px'} />
-              <div className={styles.grid2}>
-                {c[0].articles?.map((article, index) => (
-                  <>
-                    {' '}
-                    <div className={styles.mediumToBig__width}>
-                      <Article
-                        article={article}
-                        style={{
-                          minWidth: '180px'
-                        }}
-                      />
-                    </div>
-                    <div className={styles.small__width}>
-                      <Article2 article={article} />
-                    </div>
-                  </>
-                ))}
-              </div>
-            </div>
-            <div className={styles.right}>
-              <div className={styles.flex}>
-                <h2>{c[1]?.category}</h2>
-                <div className={styles.button}>See All </div>
-              </div>
-              <PBar height={'2px'} />
+              <div className={styles.right}>
+                <div className={styles.flex}>
+                  <h2>{c[1]?.category}</h2>
+                  <div className={styles.button}>See All </div>
+                </div>
+                <PBar height={'2px'} />
 
-              <div className={styles.grid2}>
-                {c[1].articles?.map((article, index) => (
-                  <>
-                    {' '}
-                    <div className={styles.mediumToBig__width}>
-                      <Article
-                        article={article}
-                        style={{
-                          minWidth: '180px'
-                        }}
-                      />
-                    </div>
-                    <div className={styles.small__width}>
-                      <Article2 article={article} />
-                    </div>
-                  </>
-                ))}
+                <div className={styles.grid2}>
+                  {c[1].articles?.map((article, index) => (
+                    <>
+                      {' '}
+                      <div className={styles.mediumToBig__width}>
+                        <Article
+                          article={article}
+                          style={{
+                            minWidth: '180px'
+                          }}
+                        />
+                      </div>
+                      <div className={styles.small__width}>
+                        <Article2 article={article} />
+                      </div>
+                    </>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </>
   )
 }
