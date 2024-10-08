9a578111da81c40f4e99aa4d03df93ea7059528a
sohanr-cyber <81401762+sohanr-cyber@users.noreply.github.com>
2024-10-08 18:12:09 +0600

    0583253

--- database/model/User.js
+++ database/model/User.js
@@ -24,11 +24,11 @@ const userSchema = new mongoose.Schema(
 
     firstNameBn: {
       type: String,
-      required: true
+      // required: true
     },
     lastNameBn: {
       type: String,
-      required: true
+      // required: true
     },
     salt: {
       type: String
